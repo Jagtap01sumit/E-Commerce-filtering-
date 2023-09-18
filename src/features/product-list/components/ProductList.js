e899eee97d56a216658674bc1a845c1697536cc5
Sumit <jagtasumit668@gmail.com>
2023-09-18 21:26:43 +0530

    mobile filter name

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -437,7 +437,7 @@ function MobileFilter({
                           <h3 className="-mx-2 -my-3 flow-root">
                             <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
                               <span className="font-medium text-gray-900">
-                                {section.title}
+                                {section.name}
                               </span>
                               <span className="ml-6 flex items-center">
                                 {open ? (
