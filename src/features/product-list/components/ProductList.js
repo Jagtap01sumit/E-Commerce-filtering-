01eb8472d5f5e762b07227b9742c2ed9c7a74cad
Sumit <jagtasumit668@gmail.com>
2023-09-24 21:29:07 +0530

    commit

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -8,7 +8,7 @@ import {
   selectCategories,
   selectBrands,
   fetchBrandsAsync,
-  fetchCategoriesAsync
+  fetchCategoriesAsync,
 } from "../productListSlice";
 import { ITEMS_PER_PAGE } from "../../../app/constants";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
@@ -46,7 +46,6 @@ const subCategories = [
   { name: "Laptop Sleeves", href: "#" },
 ];
 
-
 export default function ProductList() {
   const dispatch = useDispatch();
   const products = useSelector(selectAllProducts);
@@ -57,7 +56,7 @@ export default function ProductList() {
   const [filter, setFilter] = useState({});
   const [sort, setSort] = useState({});
   const [page, setPage] = useState(1);
- 
+
   const filters = [
     {
       id: "brand",
@@ -67,7 +66,8 @@ export default function ProductList() {
     {
       id: "category",
       name: "Category",
-      options:categories, }
+      options: categories,
+    },
   ];
   const handleFilter = (e, section, option) => {
     const newFilter = { ...filter };
@@ -99,14 +99,14 @@ export default function ProductList() {
   useEffect(() => {
     const pagination = { _page: page, _limit: ITEMS_PER_PAGE };
     dispatch(fetchProductsByFiltersAsync({ filter, sort, pagination }));
-  }, [dispatch, filter,sort, page]);
-useEffect(()=>{
-  setPage(1)
-},[totalItems,sort])
-useEffect(()=>{
-dispatch(fetchBrandsAsync());
-dispatch(fetchCategoriesAsync());
-},[])
+  }, [dispatch, filter, sort, page]);
+  useEffect(() => {
+    setPage(1);
+  }, [totalItems, sort]);
+  useEffect(() => {
+    dispatch(fetchBrandsAsync());
+    dispatch(fetchCategoriesAsync());
+  }, []);
   return (
     <div>
       <div>
@@ -221,7 +221,7 @@ function MobileFilter({
   handleFilter,
   mobileFiltersOpen,
   setMobileFiltersOpen,
-  filters
+  filters,
 }) {
   return (
     <>
@@ -339,7 +339,7 @@ function MobileFilter({
     </>
   );
 }
-function DesktopFilter({ handleFilter,filters }) {
+function DesktopFilter({ handleFilter, filters }) {
   return (
     <>
       <form className="hidden lg:block">
@@ -399,18 +399,18 @@ function DesktopFilter({ handleFilter,filters }) {
   );
 }
 function Pagination({ page, setPage, handlePage, totalItems }) {
-  const totalPages=Math.ceil(totalItems/ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
         <div
-           onClick={(e)=>handlePage(page>1? page-1:page)}
+          onClick={(e) => handlePage(page > 1 ? page - 1 : page)}
           className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Previous
         </div>
         <div
-          onClick={(e)=>handlePage(page<totalPages? page+1:page)}
+          onClick={(e) => handlePage(page < totalPages ? page + 1 : page)}
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Next
@@ -433,31 +433,29 @@ function Pagination({ page, setPage, handlePage, totalItems }) {
             aria-label="Pagination"
           >
             <div
-             onClick={(e)=>handlePage(page>1? page-1:page)}
+              onClick={(e) => handlePage(page > 1 ? page - 1 : page)}
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </div>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            {Array.from({ length: totalPages }).map(
-              (el, index) => (
-                <div
-                  onClick={(e) => handlePage(index + 1)}
-                  aria-current="page"
-                  className={`relative z-10 cursor-pointer inline-flex items-center ${
-                    index + 1 === page
-                      ? "bg-indigo-600 text-white"
-                      : "text-gray-400 "
-                  } px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-                >
-                  {index + 1}
-                </div>
-              )
-            )}
+            {Array.from({ length: totalPages }).map((el, index) => (
+              <div
+                onClick={(e) => handlePage(index + 1)}
+                aria-current="page"
+                className={`relative z-10 cursor-pointer inline-flex items-center ${
+                  index + 1 === page
+                    ? "bg-indigo-600 text-white"
+                    : "text-gray-400 "
+                } px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+              >
+                {index + 1}
+              </div>
+            ))}
 
             <div
-           onClick={(e)=>handlePage(page<totalPages? page+1:page)}
+              onClick={(e) => handlePage(page < totalPages ? page + 1 : page)}
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
